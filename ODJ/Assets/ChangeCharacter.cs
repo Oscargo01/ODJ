a94c6c36a14dd17ae3d80530622c5f1233ccf1d8
jose daniel gomez maldonado <jodagoma61@gmail.com>
2020-06-25 16:08:05 -0500

    Animaciones

---
+++ ODJ/Assets/ChangeCharacter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCharacter : MonoBehaviour
+{
+
+    public Sprite mySprite;
+
+    public Sprite mySprite2;
+
+    bool isAlive = true;
+    bool first;
+    // Start is called before the first frame update
+    void Start()
+    {
+	first=true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+if(first){
+  if (isAlive)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = mySprite;
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().sprite = mySprite2;
+            }
+first=false;
+}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isAlive)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = mySprite2;
+                isAlive = false;
+            }
+            else
+            {
+                this.GetComponent<SpriteRenderer>().sprite = mySprite;
+                isAlive = true;
+            }
+        }
+    }
+}
