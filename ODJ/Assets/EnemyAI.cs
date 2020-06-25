491b832e67571361717ee45b5b5df3a1c39d6033
José Darío Flórez <jd.florezg1@uniandes.edu.co>
2020-06-25 12:31:15 -0500

    Funcional

--- ODJ/Assets/EnemyAI.cs
+++ ODJ/Assets/EnemyAI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public float minDist;
     public float visionRadius;
     public float speed;
     public float distance;
@@ -15,45 +16,61 @@ public class EnemyAI : MonoBehaviour
     public Transform enemyPosF;
     GameObject player;
     Vector3 initialPosition;
+    int contador;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         initialPosition = transform.position;
+        contador =0;
     }
 
     void Update()
     {
         Vector3 target = initialPosition;
-
-        float dist = Vector3.Distance(player.transform.position, transform.position);
-        if (dist < visionRadius) {
-            target = player.transform.position;
+        float dist2 = Mathf.Abs(Vector3.Distance(initialPosition, transform.position));
+        if (dist2>minDist || contador>0)
+        {
             float fixedSpeed = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+            transform.position = Vector3.MoveTowards(transform.position, initialPosition, fixedSpeed);
+            contador = 1;
         }
         else
         {
-        
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
-
-            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
-
-            isGrounded = Physics2D.OverlapCircle(enemyPosF.position, checkRadius, whatIsGround);
-            if (groundInfo.collider == false || isGrounded)
+            float dist = Vector3.Distance(player.transform.position, transform.position);
+            if (dist < visionRadius)
             {
-                if (movingRight == true)
+                target = player.transform.position;
+                float fixedSpeed = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+            }
+            else
+            {
+
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+                RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+
+                isGrounded = Physics2D.OverlapCircle(enemyPosF.position, checkRadius, whatIsGround);
+                if (groundInfo.collider == false || isGrounded)
                 {
-                    transform.eulerAngles = new Vector3(0, -180, 0);
-                    movingRight = false;
-                }
-                else
-                {
-                    transform.eulerAngles = new Vector3(0, 0, 0);
-                    movingRight = true;
+                    if (movingRight == true)
+                    {
+                        transform.eulerAngles = new Vector3(0, -180, 0);
+                        movingRight = false;
+                    }
+                    else
+                    {
+                        transform.eulerAngles = new Vector3(0, 0, 0);
+                        movingRight = true;
+                    }
                 }
             }
+        
         }
-
+        if (dist2<=1)
+        {
+            contador = 0;
+        }
         Debug.DrawLine(transform.position, target, Color.green);
 
     }
