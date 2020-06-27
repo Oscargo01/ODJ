395cf6ce7dd595decf44b5c57756db37e637511c
jose daniel gomez maldonado <jodagoma61@gmail.com>
2020-06-27 18:13:22 -0500

    Animacion ataque

---
+++ ODJ/Assets/CambiarArmaAtaque.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarArmaAtaque : MonoBehaviour
+{
+
+    public Sprite mySprite;
+public float tiempo;
+
+    public Sprite mySprite2;
+
+    bool isAlive = true;
+    bool first;
+    // Start is called before the first frame update
+    void Start()
+    {
+    first=true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!isAlive)
+            {
+        StartCoroutine(ExampleCoroutine(tiempo));
+
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isAlive)
+            {
+                isAlive = false;
+            }
+            else
+            {
+                isAlive = true;
+            }
+        }
+    }
+
+    IEnumerator ExampleCoroutine(float tiempo)
+    {
+    this.GetComponent<SpriteRenderer>().sprite = mySprite2;
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(tiempo);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    this.GetComponent<SpriteRenderer>().sprite = mySprite;
+    }
+}
---
+++ ODJ/Assets/CambiarArmaAtaque2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarArmaAtaque2 : MonoBehaviour
+{
+
+    public Sprite mySprite;
+public float tiempo;
+
+    public Sprite mySprite2;
+
+    bool isAlive = true;
+    bool first;
+    // Start is called before the first frame update
+    void Start()
+    {
+this.GetComponent<SpriteRenderer>().sprite = mySprite;
+    first=true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator ExampleCoroutine(float tiempo)
+    {
+    this.GetComponent<SpriteRenderer>().sprite = mySprite2;
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(tiempo);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    this.GetComponent<SpriteRenderer>().sprite = mySprite;
+    }
+}
